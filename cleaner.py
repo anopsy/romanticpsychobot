4c9d4d87bdcc690582aef3973769a572b8f8820d
anopsy <anopsy28@gmail.com>
2023-03-11 19:35:52 +0100

    v2 cleaner

--- cleaner.py
+++ cleaner.py
@@ -2,16 +2,25 @@ import csv
 import time
 
 file= open("romanticpsycho.csv", "r")
-data = list(csv.reader(file, delimiter=","))
+data = list(csv.reader(file, delimiter=','))
 file.close()
 clean=[]
 for song in data:
     for lyrics in song:
         text = lyrics.split("<br/>")
+        for line in text:
+            if line.startswith("<"):
+                line.replace(line,"")
+            #line.replace("</div>","")
+            #line.replace("<div>","")
+            #line.replace("<div class=\"\"lyrics--text\"\">","")
         clean.append(text)
-with open('romanticpsychoclean.csv', 'w') as f:
+with open('quebott.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerows(clean)
+
+
+print(type(song), type(lyrics), type(text), type(line), type(clean))
   
 # file= open("romanticpsychoclean.csv", "r")
 # data = list(csv.reader(file, delimiter=","))
